b50aef0b5390331ba502d43885fcc6cc309ee011
Kevin Klocke <kvnklocke@gmail.com>
2025-06-13 22:05:53 +0200

    Updated

--- src/pages/game/Game.vue
+++ src/pages/game/Game.vue
@@ -1,26 +1,56 @@
 <template>
-    <Page>
-        <div class="row mt-5">
-            <div class="col">
-                <button @click="test">afaf</button>
-            </div>
+  <div>
+    <h3>Spieler Reihenfolge</h3>
+    <draggable v-model="players" item-key="id" animation="200">
+      <template #item="{ element, index }">
+        <div class="player-item">
+          <span>{{ index + 1 }}. {{ element.name }}</span>
+          <span class="points">Punkte: {{ calculatePoints(index) }}</span>
         </div>
-        <div class="row mt-3">
-            <div class="col">
-                {{ players }}
-            </div>
-        </div>
-    </Page>
+      </template>
+    </draggable>
+  </div>
 </template>
 
 <script setup lang="ts">
-import Page from '@/components/Page.vue'
-import { ref } from 'vue'
+import { ref } from 'vue';
+import draggable from 'vuedraggable';
 
-const players = ref<string[]>([])
+interface Player {
+  id: number;
+  name: string;
+}
 
-function test() {
-    const data = localStorage.getItem('player')
-    players.value = JSON.parse(data)
+const players = ref<Player[]>([
+  { id: 1, name: 'Anna' },
+  { id: 2, name: 'Ben' },
+  { id: 3, name: 'Clara' },
+  { id: 4, name: 'David' },
+]);
+
+function calculatePoints(index: number): number {
+  return players.value.length - index;
 }
 </script>
+
+<style scoped>
+.player-item {
+  display: flex;
+  justify-content: space-between;
+  padding: 8px;
+  margin-bottom: 4px;
+  background-color: #e0f7fa;
+  border-radius: 4px;
+  cursor: grab;
+  user-select: none;
+}
+.points {
+  font-weight: bold;
+  color: #00796b;
+}
+</style>
+
+
+
+
+
